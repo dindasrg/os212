cdc1994fd41668efd2e51a0f5a9d3ea9a37ef466
Dinda Adriani Siregar <74523925+dindasrg@users.noreply.github.com>
2021-09-18 21:14:22 +0700

    fix myscript.sh

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# REV00 Sun 12 Sep 2021 03:10:00 WIB
+# START Sun 12 Sep 2021 03:10:00 WIB
+
+FILES="my*.txt my*.sh"
+SHA="SHA256SUM"
+
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
